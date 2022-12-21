21e364f1d04ac7dc68310ab99c35678f92fe95a0
Oleg AKsenov <takun2000@gmail.com>
2022-12-21 22:09:53 +0300

    добавлен первый урок, задача 2

---
+++ lesson_1/task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 5,b = 7;
+if ( a > b)
+{
+    int max = a;
+    Console.WriteLine("Максимальное число:" + max);
+} else
+{
+    int max = b;
+    Console.WriteLine("Максимальное число:" + max);
+}
+
