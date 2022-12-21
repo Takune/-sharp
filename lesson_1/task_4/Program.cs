09ba2ba816640d095efafd7e822c42880a0e4e92
Oleg AKsenov <takun2000@gmail.com>
2022-12-21 22:59:43 +0300

    добавлено 4 задание

---
+++ lesson_1/task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 44,b = 5,c = 78;
+if ( a > b)
+{
+    int max = a;
+    Console.WriteLine("Максимальное число:" + max);
+} else if(b > c)
+{
+    int max = b;
+    Console.WriteLine("Максимальное число:" + max);
+} else
+{
+    int max = c;
+    Console.WriteLine("Максимальное число:" + max);
+}
