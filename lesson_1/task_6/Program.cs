11250a71fa380cd6d5bd1eaa9394252b2a17bfc2
Oleg AKsenov <takun2000@gmail.com>
2022-12-21 22:59:52 +0300

    добавлено 6 задание

---
+++ lesson_1/task_6/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Проверка четность числа");
+Console.Write("Введите число: " );
+int num = Convert.ToInt32 (Console.ReadLine());
+if (num % 2 == 0) 
+{
+    Console.WriteLine("число " + num + " четное");
+} else
+{
+    Console.WriteLine("число " + num + " нечетное");
+}
+
+
